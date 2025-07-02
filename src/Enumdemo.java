6d0da13cbc26119bd842da8650da12e2a3e680f8
adi3489 <07aditya.cn@gmail.com>
2025-07-02 13:43:47 +0530

    throw

---
+++ src/Enumdemo.java
@@ -0,0 +1,10 @@
+public enum Enumdemo {
+
+    nasic,intermediate,Expert;
+    public static void main(String[] args) {
+        Enumdemo obj = Enumdemo.nasic;
+        Enumdemo obj1 = Enumdemo.Expert;
+        System.out.println(obj1);
+
+    }
+}
