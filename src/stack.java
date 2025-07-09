5b60821acfa9bbe8bfa3bb43c539f3894643212c
adi3489 <07aditya.cn@gmail.com>
2025-07-09 10:50:06 +0530

    list stack

---
+++ src/stack.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.remove(2);
+        s.push(3);
+        s.pop();
+        System.out.println(s);
+    }
+}
