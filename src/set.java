c9a3bdf5d9d602d2df3a5b3dcff642c44c1cdac8
adi3489 <07aditya.cn@gmail.com>
2025-07-16 13:00:21 +0530

    set

--- src/set.java
+++ src/set.java
@@ -1,11 +1,15 @@
+import java.security.Key;
 import java.util.*;
 
 public class set {
     public static void main(String[] args) {
-        HashSet<Integer> list = new HashSet<>();
-        list.add(5);
-        list.add(4);
-        list.add(5);
-        System.out.println(list);
+        HashMap<Integer, String> list = new HashMap<>();
+        list.put(5, "Aditya");
+        list.put(4, "Aditya");
+        list.put(2, "Piyush");
+        list.forEach((Integer Key, String Value) -> {
+            if(Value.equals("Piyush"))
+                System.out.println(Key);
+        });
     }
 }
