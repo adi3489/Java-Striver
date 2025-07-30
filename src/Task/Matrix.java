fb6faa9d8f9c3667c56523fcecea42fb971fdb79
adi3489 <07aditya.cn@gmail.com>
2025-07-30 10:27:59 +0530

    Matrix

--- src/Task/Matrix.java
+++ src/Task/Matrix.java
@@ -1,6 +1,5 @@
 package Task;
 
-
 import java.util.Scanner;
 
 public class Matrix {
@@ -9,24 +8,35 @@ public class Matrix {
         int[][] matrix2 = new int[3][3];
 
         Scanner s = new Scanner(System.in);
-//normal matrix input
+
+        System.out.println("Enter elements of 3x3 matrix:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 matrix1[i][j] = s.nextInt();
             }
         }
 
-        //y transposee matrix
+        // Print original matrix
+        System.out.println("Original Matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Transpose logic
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 matrix2[j][i] = matrix1[i][j];
             }
         }
 
+        // Print transposed matrix
+        System.out.println("Transposed Matrix:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.println(matrix2[i][j]);//print m dikkat aayi
-
+                System.out.print(matrix2[i][j] + " ");
             }
             System.out.println();
         }
